47d41644638117155417c4a9673077a88cf5db0e
madhu <dmpathani@gmail.com>
2024-07-04 22:53:09 +0530

    changed ui

--- lib/chat.dart
+++ lib/chat.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_generative_ai/google_generative_ai.dart';
 
@@ -32,23 +33,6 @@ class ChatbotState extends State<Chatbot> {
     });
   }
 
-  Widget buildResponse(String text) {
-    List<TextSpan> textSpans = [];
-
-    List<String> parts = text.split('*');
-    for (int i = 0; i < parts.length; i++) {
-      if (i % 2 == 0) {
-        textSpans.add(TextSpan(text: parts[i], style: TextStyle(color: Colors.black)));
-      } else {
-        textSpans.add(TextSpan(text: parts[i], style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold)));
-      }
-    }
-
-    return RichText(
-      text: TextSpan(children: textSpans),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
@@ -57,45 +41,44 @@ class ChatbotState extends State<Chatbot> {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.secondaryContainer,
       floatingActionButton: FloatingActionButton(
-          backgroundColor: Theme.of(context).colorScheme.onPrimaryContainer,
-          child: Icon(Icons.headphones, color: Theme.of(context).colorScheme.onSecondary),
+          backgroundColor: Theme.of(context).colorScheme.primary,
+          child: Icon(CupertinoIcons.mic, color: Theme.of(context).colorScheme.onSecondary),
           onPressed: () {
             generateContent(_controller.text);
             _controller.clear();
           }),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          Icon(
-            Icons.android,
-            color: Theme.of(context).colorScheme.onPrimaryContainer,
-            size: 100,
+      body: Container(
+        width: double.maxFinite,
+        height: double.maxFinite,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Color(0xff80e0e0), Color(0xff736cc7)],
           ),
-          Container(
-            height: height * 0.5,
-            width: width * 0.8,
-            padding: EdgeInsets.all(20),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.all(Radius.circular(20)),
-              color: Theme.of(context).colorScheme.inversePrimary,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Image(
+              image: AssetImage(
+                "asset/logo.png"
+              )
             ),
-            child: SingleChildScrollView(
-              child: buildResponse(responseText),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 80, left: 20, right: 20),
+              child: TextField(
+                textInputAction: TextInputAction.go,
+                controller: _controller,
+                decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(),
+                    labelText: "Enter your text",
+                    hintText: ""),
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(bottom: 80, left: 20, right: 20),
-            child: TextField(
-              textInputAction: TextInputAction.go,
-              controller: _controller,
-              decoration: InputDecoration(
-                  enabledBorder: OutlineInputBorder(),
-                  labelText: "Enter your text",
-                  hintText: ""),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
--- lib/depth.dart
+++ lib/depth.dart
@@ -1,5 +1,6 @@
 import 'dart:typed_data';
 import 'package:camera/camera.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_generative_ai/google_generative_ai.dart';
 import 'dart:convert';
@@ -20,7 +21,7 @@ class _DepthState extends State<Depth> {
   late Future<void> cameraValue;
   Uint8List? imageData;
   bool isRearCamera = true;
-  String responseText = "nothing";
+  String responseText = "no caption yet";
 
   final model = GenerativeModel(
     model: 'gemini-1.5-flash-latest',
@@ -40,8 +41,6 @@ class _DepthState extends State<Depth> {
     });
 
     // Prepare your API authentication
-    final uname = 'acc_8f0ef6960cd9068';
-    final pword = '571bbca97e4222db02150267af7c069c';
     final authn = 'Basic YWNjXzhmMGVmNjk2MGNkOTA2ODo1NzFiYmNhOTdlNDIyMmRiMDIxNTAyNjdhZjdjMDY5Yw==';
     
     // Define your API endpoint
@@ -112,7 +111,7 @@ class _DepthState extends State<Depth> {
             shape: const CircleBorder(),
             onPressed: takePicture,
             child: const Icon(
-              Icons.question_mark,
+              CupertinoIcons.camera,
               size: 40,
               color: Colors.black87,
             ),
@@ -120,74 +119,56 @@ class _DepthState extends State<Depth> {
         ],
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      body: Stack(
-        children: [
-          FutureBuilder(
-            future: cameraValue,
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.done) {
-                return Center(
-                  child: Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(20)),
-                    ),
-                    width: width * 0.9,
-                    height: height * 0.87,
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.all(Radius.circular(20)),
-                      child: CameraPreview(cameraController),
-                    ),
-                  ),
-                );
-              } else {
-                return const Center(
-                  child: CircularProgressIndicator(),
-                );
-              }
-            },
+      body: Container(
+        width: double.maxFinite,
+        height: double.maxFinite,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Color(0xff80e0e0), Color(0xff736cc7)],
           ),
-          Align(
-            alignment: Alignment.bottomLeft,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 7, bottom: 75),
-                    child: Container(
-                      height: 100,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: imageData != null
-                          ? Padding(
-                              padding: const EdgeInsets.all(2),
-                              child: ClipRRect(
-                                borderRadius: BorderRadius.circular(10),
-                                child: Image.memory(
-                                  imageData!,
-                                  height: 100,
-                                  width: 100,
-                                  fit: BoxFit.cover,
-                                ),
-                              ),
-                            )
-                          : Container(),
-                    ),
-                  ),
-                )
-              ],
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Container(
+              padding: EdgeInsets.all(width*0.05),
+              
+              height: height*0.095,
+              width: width*0.9,
+              decoration: BoxDecoration(
+                color: Color.fromARGB(255, 150, 144, 214),
+                borderRadius: BorderRadius.all(Radius.circular(20))
+              ),
+              child: SingleChildScrollView(child: Center(child: Text(responseText))),
             ),
-          ),
-          Container(
-            padding: EdgeInsets.all(width*0.05),
-            color: Theme.of(context).colorScheme.secondaryContainer,
-            height: responseText == "nothing"? 0 : 100,
-            width: responseText == "nothing"? 0 : width,
-            child: SingleChildScrollView(child: Center(child: Text(responseText))),
-          )
-        ],
+            FutureBuilder(
+              future: cameraValue,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.done) {
+                  return Center(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(20)),
+                      ),
+                      width: width * 0.9,
+                      height: height * 0.75,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.all(Radius.circular(20)),
+                        child: CameraPreview(cameraController),
+                      ),
+                    ),
+                  );
+                } else {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
