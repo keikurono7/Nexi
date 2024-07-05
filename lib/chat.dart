1d59ecccd25f85b7d854a962e02be56831311f9b
madhu <dmpathani@gmail.com>
2024-07-05 07:31:36 +0530

    text implemented

--- lib/chat.dart
+++ lib/chat.dart
@@ -1,9 +1,10 @@
+import 'dart:convert';
+
 import 'package:avatar_glow/avatar_glow.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_generative_ai/google_generative_ai.dart';
-import 'package:speech_to_text/speech_to_text.dart' as stt;
-import 'package:flutter_tts/flutter_tts.dart';
+import 'package:http/http.dart' as http;
 
 class Chatbot extends StatefulWidget {
   const Chatbot({super.key});
@@ -13,87 +14,95 @@ class Chatbot extends StatefulWidget {
 }
 
 class ChatbotState extends State<Chatbot> {
-  late stt.SpeechToText _speech;
-  late FlutterTts _flutterTts;
-  bool _isListening = false;
-  String _text = "";
-  String responseText = "Hey there, How can I help you today?";
-
-  final model = GenerativeModel(
-    model: 'gemini-1.5-flash-latest',
-    apiKey: 'AIzaSyBlbaYo1uoMCYz2FGoFfvilZ9oPmy-Mcw8',
-  );
-
-  @override
-  void initState() {
-    super.initState();
-    _speech = stt.SpeechToText();
-    _flutterTts = FlutterTts();
-  }
+  final TextEditingController _controller = TextEditingController();
+  List<String> past = ["hii", "heyy there"];
+  String responseText = "Heyy there, How can I help you today?";
+  final String apiKey = 'AIzaSyBlbaYo1uoMCYz2FGoFfvilZ9oPmy-Mcw8'; 
 
   Future<void> generateContent(String prompt) async {
-    String conversationContext = "Continue the conversation as a therapist would and only answer the question. The person asks $prompt";
-    final content = [Content.text(conversationContext)];
-    final response = await model.generateContent(content);
-    setState(() {
-      responseText = response.text ?? "No response";
-      _speak(responseText);
-    });
-  }
+    past.add(prompt);
 
-  void _startListening() async {
-    bool available = await _speech.initialize(
-      onStatus: (val) => print('onStatus: $val'),
-      onError: (val) => print('onError: $val'),
-    );
-    if (available) {
-      setState(() => _isListening = true);
-      _speech.listen(
-        onResult: (val) => setState(() {
-          _text = val.recognizedWords;
-          if (val.hasConfidenceRating && val.confidence > 0) {
-            print('Recognized Words: $_text');  // Add this line for debugging
-            generateContent(_text);
-            _stopListening();
-          }
-        }),
-        listenFor: Duration(seconds: 10),
-        pauseFor: Duration(seconds: 5),
-        localeId: 'en_US',
-        onSoundLevelChange: (val) => print('Sound Level: $val'),  // Add this line for debugging
-        cancelOnError: true,
-        listenMode: stt.ListenMode.confirmation,
+    String conversationContext = "";
+    for (int i = 0; i < past.length - 1; i += 2) {
+      conversationContext +=
+          "[ person : ${past[i]} ] [ gemini response : ${past[i + 1]} ] ";
+    }
+    conversationContext +=
+        "Continue the conversation as a therapist would and only answer the question. The person asks $prompt";
+
+    final headers = {
+      'Content-Type': 'application/json',
+    };
+
+    final json_data = {
+      'contents': [
+        {
+          'parts': [
+            {
+              'text': conversationContext,
+            },
+          ],
+        },
+      ],
+    };
+
+    try {
+      final response = await http.post(
+        Uri.parse(
+            'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=$apiKey'),
+        headers: headers,
+        body: json.encode(json_data),
       );
-    } else {
-      print('The user has denied the use of speech recognition.');
+
+      if (response.statusCode == 200) {
+        final responseBody = json.decode(response.body);
+        final generatedText =
+            responseBody["candidates"][0]["content"]["parts"][0]["text"];
+        past.add(generatedText);
+
+        setState(() {
+          responseText = generatedText ?? "No response";
+        });
+      } else {
+        throw Exception('Failed to fetch data');
+      }
+    } catch (e) {
+      print('Error: $e');
+      setState(() {
+        responseText = "Error occurred. Please try again.";
+      });
     }
   }
 
-  void _stopListening() {
-    setState(() => _isListening = false);
-    _speech.stop();
-  }
+  Widget buildResponse(String text) {
+    List<TextSpan> textSpans = [];
 
-  Future<void> _speak(String text) async {
-    await _flutterTts.speak(text);
+    List<String> parts = text.split('*');
+    for (int i = 0; i < parts.length; i++) {
+      if (i % 2 == 0) {
+        textSpans.add(TextSpan(text: parts[i], style: TextStyle(color: Colors.black)));
+      } else {
+        textSpans.add(TextSpan(text: parts[i], style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold)));
+      }
+    }
+
+    return RichText(
+      text: TextSpan(children: textSpans),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     double height = MediaQuery.of(context).size.height;
-
+    double width = MediaQuery.of(context).size.width;
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.secondaryContainer,
       floatingActionButton: FloatingActionButton(
-        backgroundColor: Color(0xff98bdde),
-        shape: const CircleBorder(),
-        child: Icon(_isListening ? CupertinoIcons.mic_off : CupertinoIcons.mic, color: Colors.white),
+        backgroundColor: Theme.of(context).colorScheme.onPrimaryContainer,
+        child: Icon(CupertinoIcons.pen, color: Theme.of(context).colorScheme.onSecondary),
         onPressed: () {
-          if (_isListening) {
-            _stopListening();
-          } else {
-            _startListening();
-          }
+          generateContent(_controller.text);
+          _controller.clear();
         },
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
@@ -108,20 +117,40 @@ class ChatbotState extends State<Chatbot> {
           ),
         ),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             AvatarGlow(
-              child: Image(
-                image: AssetImage("asset/logo.png"),
+              child: SizedBox(
+                height: height * 0.25,
+                child: Image(
+                  image: AssetImage("asset/logo.png"),
+                ),
+              ),
+            ),
+            Container(
+              height: height * 0.2,
+              width: width * 0.8,
+              padding: EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+                color: Theme.of(context).colorScheme.inversePrimary,
+              ),
+              child: SingleChildScrollView(
+                child: buildResponse(responseText),
               ),
             ),
             Padding(
               padding: const EdgeInsets.all(16.0),
-              child: Text(
-                responseText,
-                style: TextStyle(fontSize: 16, color: Colors.white),
+              child: TextField(
+                textInputAction: TextInputAction.go,
+                controller: _controller,
+                decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(),
+                  labelText: "Enter your text",
+                  hintText: "",
+                ),
               ),
-            ),
+            )
           ],
         ),
       ),
